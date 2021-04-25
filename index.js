338e9c1bc767da114a2a9ccf5983819947ad78a9
Bilal-Nizamani <76642491+Bilal-Nizamani@users.noreply.github.com>
2021-04-24 20:15:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+
+app.post("/", function(req, res){
+
+  
+  const  name = req.body.name;
+
+
+  res.redirect("/");
+
+
+
+});
+
+app.get("/", function(req, res){
+  res.render("index.ejs", {
+    });
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
